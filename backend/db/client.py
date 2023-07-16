cf485b6c6c8bc5f22dbc4d9773475cb5f848519c
Franco Giovanni Cirielli <ciriellifrancogiovanni@gmail.com>
2023-07-15 21:00:59 -0300

    Login y registro terminado

--- backend/db/client.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-#Base de datos local
-#db_client = MongoClient().local
-
-#Base de datos remota
-db_client = MongoClient("mongodb+srv://FrancoCirielli16:Franco2002@usercryptoscoin.gxkjmvk.mongodb.net/?retryWrites=true&w=majority").test
