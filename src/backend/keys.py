36e1eb737ea48c0be0e62e348cc22d208b68876c
Franco Giovanni Cirielli <ciriellifrancogiovanni@gmail.com>
2023-08-05 17:42:20 -0300

    API

---
+++ src/backend/keys.py
@@ -0,0 +1,7 @@
+SENDGRID_API_KEY= "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"
+MONGO_DB_KEY = "mongodb+srv://FrancoCirielli16:Franco2002@usercryptoscoin.gxkjmvk.mongodb.net/?retryWrites=true&w=majority"
+COIN_API = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+COIN_API_TEST = "https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_DURATION = 10
+SECRET = "201d573bd7d1344d3a3bfce1550b69102fd11be3db6d379508b6cccc58ea230b"
